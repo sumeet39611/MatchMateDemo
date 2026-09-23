4004c75f42430a36791f46122fd1c8ca1000ed2b
Sumeet Jagtap <sumeetjagtap@Sumeets-Laptop.local>
2026-09-23 15:25:53 +0530

    Initial Xcode project setup

---
+++ MatchMateDemo/MatchMateDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchMateDemo
+//
+//  Created by Sumeet Jagtap on 23/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatchMateDemo/MatchMateDemo/MatchMateDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateDemoApp.swift
+//  MatchMateDemo
+//
+//  Created by Sumeet Jagtap on 23/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
