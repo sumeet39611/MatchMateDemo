99ac2d115027218109365d30cedfdcf29e8143b6
sumeet39611 <sumeet39611@gmail.com>
2026-09-23 18:33:21 +0530

    status  view added

---
+++ MatchMateDemo/MatchMateDemo/Views/StatusView.swift
@@ -0,0 +1,18 @@
+//
+//  StatusView.swift
+//  MatchMateDemo
+//
+//  Created by Sumeet Jagtap on 23/09/26.
+//
+
+import SwiftUI
+
+struct StatusView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatusView()
+}
